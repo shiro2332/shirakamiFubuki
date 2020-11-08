d582388c428191c76840442586d1636946094661
shiro2332 <68656146+shiro2332@users.noreply.github.com>
2020-11-09 01:15:14 +0800

    Update playAudio.js

--- commands/audio/playAudio.js
+++ commands/audio/playAudio.js
@@ -21,7 +21,7 @@ module.exports = class PlayAudioCommand extends Commando.Command {
 
     voice.channel.join().then((connection) => {
       //connection.play(path.join(__dirname, 'angery.mp3'))
-      connection.play("https://drive.google.com/file/d/18jYVzK-unDcylyAG8lzM9Xnkoqs3_qi0/view?usp=sharing")
+      connection.play("http://download1642.mediafire.com/l5m95wz4yeig/q05u8i8jze9l77l/angery.mp3")
     })
   }
 }
