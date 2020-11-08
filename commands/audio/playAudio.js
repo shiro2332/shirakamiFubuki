78697795d5b532bd8c564720a48e56a2a5678ad9
shiro2332 <68656146+shiro2332@users.noreply.github.com>
2020-11-09 00:07:43 +0800

    Update playAudio.js

--- commands/audio/playAudio.js
+++ commands/audio/playAudio.js
@@ -20,7 +20,8 @@ module.exports = class PlayAudioCommand extends Commando.Command {
     }
 
     voice.channel.join().then((connection) => {
-      connection.play("http://download2282.mediafire.com/nj9brysjmr3g/q05u8i8jze9l77l/angery.mp3");
+      //connection.play("http://download2282.mediafire.com/nj9brysjmr3g/q05u8i8jze9l77l/angery.mp3");
+      const dispatcher = connection.play('http://download2282.mediafire.com/nj9brysjmr3g/q05u8i8jze9l77l/angery.mp3', {volume: 0.5,});
     })
   }
 }
