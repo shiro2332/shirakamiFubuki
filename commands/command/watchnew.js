86bc0ac911ae2aeb9944030dca42edd0ae3bc63f
shiro2332 <68656146+shiro2332@users.noreply.github.com>
2020-11-09 10:04:47 +0800

    Update watchnew.js

--- commands/command/watchnew.js
+++ commands/command/watchnew.js
@@ -1,5 +1,5 @@
 const { Command } = require('discord.js-commando')
-const config = require('./config.json')
+const config = require('../../config.json')
 const client = new Command.CommandoClient({
 	owner: "299176846132641792",
 	commandPrefix: config.prefix
