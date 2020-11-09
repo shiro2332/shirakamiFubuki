f9e991271eeeee783300cc46e8987c08593222cb
shiro2332 <68656146+shiro2332@users.noreply.github.com>
2020-11-09 13:06:38 +0800

    Update hello.js

--- commands/command/hello.js
+++ commands/command/hello.js
@@ -11,6 +11,6 @@ module.exports = class BasicCommand extends Command {
 	}
 
 	run(message) {
-		return message.channel.send('Hi friends ' + message.author.toString() + `$<:shira:775194767247409163>`)
+		return message.channel.send('Hi friends ' + message.author.toString() + `$<:shira:775192841580052521>`)
 	}
 };
