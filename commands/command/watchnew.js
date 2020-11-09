f9b34071477f833aa35a792a3f8b29ab109ffb54
shiro2332 <68656146+shiro2332@users.noreply.github.com>
2020-11-09 10:21:57 +0800

    Update watchnew.js

--- commands/command/watchnew.js
+++ commands/command/watchnew.js
@@ -18,7 +18,10 @@ module.exports = class BasicCommand extends Command {
 			var livelist = Array("フブキCh。白上フブキ", "Botan Ch.獅白ぼたん", "Suisei Channel", "Korone Ch. 戌神ころね")
 			var live = livelist[Math.floor(Math.random() * livelist.length)]
 
-			client.user.setActivity(live,{ type: 'WATCHING'})
+			client.on("ready", () => {
+				client.user.setActivity(live,{ type: 'WATCHING'})
+			});
+			
 		}
 	}
 };
