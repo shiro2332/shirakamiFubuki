4582c077f0d30d7817cd092d313e1514fa899665
shiro2332 <68656146+shiro2332@users.noreply.github.com>
2020-11-09 20:09:00 +0800

    Update hello.js

--- commands/command/hello.js
+++ commands/command/hello.js
@@ -12,7 +12,7 @@ module.exports = class BasicCommand extends Command {
 	}
 
 	run(message) {
-		message.channel.send('test' + emoji.cha + emoji.ye)
+		message.channel.send('test' + emoji.angry1 + emoji.ye)
 		return message.channel.send(emoji.shira + ' ' + message.author.toString() + ' ' + 'Hi friends, tea!!' + ' ' + emoji.cha + ' ' + emoji.ye)
 
 	}
