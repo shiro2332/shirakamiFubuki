06554013789e6177f718350eae5199b6b731f43c
shiro2332 <68656146+shiro2332@users.noreply.github.com>
2020-11-09 14:18:28 +0800

    yoru ni kakeru

---
+++ commands/audio/yoru.js
@@ -0,0 +1,26 @@
+const Commando = require('discord.js-commando')
+const path = require('path')
+
+module.exports = class PlayAudioCommand extends Commando.Command {
+  constructor(client) {
+    super(client, {
+      name: 'yoru',
+      group: 'audio',
+      memberName: 'yoru',
+      description: 'Play Yoru ni Kakeru',
+    })
+  }
+
+  async run(message) {
+    const { voice } = message.member
+
+    if (!voice.channelID) {
+      message.reply('You must be in a voice channel')
+      return
+    }
+
+    voice.channel.join().then((connection) => {
+      connection.play(path.join(__dirname, 'audio/yoruSong.mp3'))
+    })
+  }
+}
