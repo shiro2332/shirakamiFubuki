e39bc3ee44eb5829cf7028c6e1128d6d9cb60282
shiro2332 <68656146+shiro2332@users.noreply.github.com>
2020-11-09 08:48:30 +0800

    Added scatman

---
+++ commands/audio/scatman.js
@@ -0,0 +1,27 @@
+const Commando = require('discord.js-commando')
+const path = require('path')
+
+module.exports = class PlayAudioCommand extends Commando.Command {
+  constructor(client) {
+    super(client, {
+      name: 'scatman',
+      group: 'audio',
+      memberName: 'scatman',
+      description: 'S C A T M A N',
+    })
+  }
+
+  async run(message) {
+    const { voice } = message.member
+
+    if (!voice.channelID) {
+      message.reply('You must be in a voice channel')
+      return
+    }
+
+    voice.channel.join().then((connection) => {
+      connection.play(path.join(__dirname, 'audio/scatman.mp3'))
+      //connection.play("http://download1642.mediafire.com/l5m95wz4yeig/q05u8i8jze9l77l/angery.mp3")
+    })
+  }
+}
