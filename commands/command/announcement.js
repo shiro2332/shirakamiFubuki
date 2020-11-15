eb2681cc0ada30283a659351c912211a7760a36d
shiro2332 <68656146+shiro2332@users.noreply.github.com>
2020-11-15 23:06:21 +0800

    Update announcement.js

--- commands/command/announcement.js
+++ commands/command/announcement.js
@@ -13,6 +13,7 @@ module.exports = class BasicCommand extends Command {
 
 	run(message) {
 		if(message.member.hasPermission("Immortal")){
+			message.channel.delete(1000);
 			message.channel.send("__**ANNOUNCEMENT**__ \n \n `Testing 1 2 3`")
 			
 		} else {
