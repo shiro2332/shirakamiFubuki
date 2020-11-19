ed81a945d6f71ae606b35bbc13157d694db2dae4
shiro2332 <68656146+shiro2332@users.noreply.github.com>
2020-11-19 15:55:58 +0800

    Update fbk.js

--- commands/command/fbk.js
+++ commands/command/fbk.js
@@ -1,5 +1,6 @@
 const { Command } = require('discord.js-commando')
-const client = Command.CommandoClient()
+const Commando = require('discord.js-commando')
+const client = Commando.CommandoClient()
 
 module.exports = class BasicCommand extends Command {
 	constructor(client) {
