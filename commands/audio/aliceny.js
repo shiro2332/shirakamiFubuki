b287aad8099b5bdd2de617a799f9e202865f171c
shiro2332 <68656146+shiro2332@users.noreply.github.com>
2020-11-19 20:44:36 +0800

    Update aliceny.js

--- commands/audio/aliceny.js
+++ commands/audio/aliceny.js
@@ -12,7 +12,7 @@ module.exports = class PlayAudioCommand extends Commando.Command {
   }
 
   async run(message) {
-    if (msg.content.includes('!!suisei alice')){
+    if (message.content.includes('!!suisei alice')){
       	const { voice } = message.member
 
       	if (!voice.channelID) {
@@ -23,7 +23,8 @@ module.exports = class PlayAudioCommand extends Commando.Command {
       	voice.channel.join().then((connection) => {
         	connection.play(path.join(__dirname, '../../assets/song/alice.mp3'))
       	})
-    }else if (msg.content.includes('!!suisei saga')){
+
+    }else if (message.content.includes('!!suisei saga')){
       	const { voice } = message.member
 
       	if (!voice.channelID) {
