6b82c13f23ffe31407590cb4620a3820e050ade1
shiro2332 <68656146+shiro2332@users.noreply.github.com>
2020-11-19 15:58:38 +0800

    Update fbk.js

--- commands/command/fbk.js
+++ commands/command/fbk.js
@@ -1,20 +1,25 @@
 const { Command } = require('discord.js-commando')
 const Commando = require('discord.js-commando')
-const client = Commando.CommandoClient()
+const client = new Commando.CommandoClient({
+	owner: "299176846132641792",
+	commandPrefix: config.prefix
+	})
 
-module.exports = class BasicCommand extends Command {
-	constructor(client) {
-		super(client, {
-			name: 'fbk',
-			group: 'command',
-			memberName: 'fbk',
-			description: 'Become fubuki',
-		});
-	}
+client.on('ready', async () => {
+	module.exports = class BasicCommand extends Command {
+		constructor(client) {
+			super(client, {
+				name: 'fbk',
+				group: 'command',
+				memberName: 'fbk',
+				description: 'Become fubuki',
+			});
+		}
 
-	async run(message) {
-		//avatar = 'https://cdn.discordapp.com/attachments/778860853835464704/778866435731619860/fbk.jpg';
-		client.user.setAvatar('https://cdn.discordapp.com/attachments/778860853835464704/778866435731619860/fbk.jpg')
-		client.user.setUsername("Shirakami Fubuki")
+		async run(message) {
+			//avatar = 'https://cdn.discordapp.com/attachments/778860853835464704/778866435731619860/fbk.jpg';
+			client.user.setAvatar('https://cdn.discordapp.com/attachments/778860853835464704/778866435731619860/fbk.jpg')
+			client.user.setUsername("Shirakami Fubuki")
+		}
 	}
 };
