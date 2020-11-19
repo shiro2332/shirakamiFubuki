b6e7129fd022f79029f2ffdcd7afdfd07b38c3e1
shiro2332 <68656146+shiro2332@users.noreply.github.com>
2020-11-19 21:28:57 +0800

    Update haachama command.js

--- commands/audio/haachama command.js
+++ commands/audio/haachama command.js
@@ -22,9 +22,8 @@ module.exports = class PlayAudioCommand extends Commando.Command {
 
       		voice.channel.join().then((connection) => {
           		connection.play(path.join(__dirname, '../../assets/audio/haachama.mp3'))
-          		message.reply("Haachama cooking in 60s.")
+          		message.send("Haachama cooking in 60s.")
         		connection.play(path.join(__dirname, '../../assets/audio/hpasta1.mp3'))
-          		voice.channel.leave()
       		})
     	}
     }
