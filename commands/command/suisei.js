b6ef69f2412b1a4078d3651780171ef5b1164034
shiro2332 <68656146+shiro2332@users.noreply.github.com>
2020-11-19 14:47:06 +0800

    Update suisei.js

--- commands/command/suisei.js
+++ commands/command/suisei.js
@@ -1,7 +1,4 @@
 const { Command } = require('discord.js-commando')
-const js = require('discord.js')
-
-const client = new js.Client()
 
 module.exports = class BasicCommand extends Command {
 	constructor(client) {
@@ -15,7 +12,7 @@ module.exports = class BasicCommand extends Command {
 
 	async run(client, message) {
 		var avatar = './suisei.png'
-		client.user.setAvatar(avatar)
-		client.user.setUsername("Hoshimachi Suisei")
+		user.setAvatar(avatar)
+		user.setUsername("Hoshimachi Suisei")
 	}
 };
