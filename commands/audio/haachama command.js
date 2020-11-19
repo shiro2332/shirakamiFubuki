a18735880b783df9f55d75f4d7f0384472dde070
shiro2332 <68656146+shiro2332@users.noreply.github.com>
2020-11-19 21:23:23 +0800

    Update haachama command.js

--- commands/audio/haachama command.js
+++ commands/audio/haachama command.js
@@ -2,29 +2,30 @@ const Commando = require('discord.js-commando')
 const path = require('path')
 
 module.exports = class PlayAudioCommand extends Commando.Command {
-  constructor(client) {
-    super(client, {
-      name: 'haachama',
-      group: 'audio',
-      memberName: 'haachama',
-      description: 'u',
-    })
-  }
+  	constructor(client) {
+    	super(client, {
+      	name: 'haachama',
+      	group: 'audio',
+      	memberName: 'haachama',
+      	description: 'u',
+    	})
+  	}
 
-  async run(message) {
-    if (message.content.includes('!!haachama cookpasta')){
-      	const { voice } = message.member
+  	async run(message) {
+    	if (message.content.includes('!!haachama cookpasta')){
+      		const { voice } = message.member
 
-      	if (!voice.channelID) {
-        	message.reply('You must be in a voice channel')
-        	return
-      	}
+      		if (!voice.channelID) {
+        		message.reply('You must be in a voice channel')
+        		return
+      		}
 
-      	voice.channel.join().then((connection) => {
-          connection.play(path.join(__dirname, '../../assets/audio/haachama.mp3'))
-          msg.reply("Haachama cooking in 60s.")
-        	connection.play(path.join(__dirname, '../../assets/audio/hpasta1.mp3'))
-          voice.channel.leave()
-      	})
+      		voice.channel.join().then((connection) => {
+          		connection.play(path.join(__dirname, '../../assets/audio/haachama.mp3'))
+          		msg.reply("Haachama cooking in 60s.")
+        		connection.play(path.join(__dirname, '../../assets/audio/hpasta1.mp3'))
+          		voice.channel.leave()
+      		})
+    	}
     }
-}
+}	
