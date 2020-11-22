0f0ab8f5e856b0d841a322c84c89770bd65d9399
shiro2332 <68656146+shiro2332@users.noreply.github.com>
2020-11-22 16:06:58 +0800

    convert

--- commands/audio/aqua command.js
+++ commands/audio/aqua command.js
@@ -12,44 +12,30 @@ module.exports = class PlayAudioCommand extends Commando.Command {
   }
 
   async run(message) {
+    function playAudio(filepath){
+        const { voice } = message.member
+
+        if (!voice.channelID) {
+            message.reply('You must be in a voice channel')
+            return
+        }
+
+        voice.channel.join().then((connection) => {
+            connection.play(path.join(__dirname, filepath))
+        })
+    }
+
     if (message.content.includes('!!aqua forthewin')){
-      	const { voice } = message.member
-
-      	if (!voice.channelID) {
-        	message.reply('You must be in a voice channel')
-        	return
-      	}
-
-      	voice.channel.join().then((connection) => {
-        	connection.play(path.join(__dirname, '../../assets/song/aqua/forthewin.mp3'))
-      	})
+      	playAudio('../../assets/song/aqua/forthewin.mp3')
 
     }
 
     else if (message.content.includes('!!aqua iropalette')){
-      	const { voice } = message.member
-
-      	if (!voice.channelID) {
-        	message.reply('You must be in a voice channel')
-        	return
-      	}
-
-      	voice.channel.join().then((connection) => {
-        	connection.play(path.join(__dirname, '../../assets/song/aqua/iropallete.mp3'))
-      	})
+      	playAudio('../../assets/song/aqua/iropallete.mp3')
     }
 
     else if (message.content.includes('!!aqua trackmaker')){
-      	const { voice } = message.member
-
-      	if (!voice.channelID) {
-        	message.reply('You must be in a voice channel')
-        	return
-      	}
-
-      	voice.channel.join().then((connection) => {
-        	connection.play(path.join(__dirname, '../../assets/song/aqua/indoortrackmaker.mp3'))
-      	})
+      	playAudio( '../../assets/song/aqua/indoortrackmaker.mp3')
     }
   }
 }
--- commands/audio/haachama command.js
+++ commands/audio/haachama command.js
@@ -12,55 +12,38 @@ module.exports = class PlayAudioCommand extends Commando.Command {
   	}
 
   	async run(message) {
+  		function playAudio(filepath){
+	        const { voice } = message.member
+
+	        if (!voice.channelID) {
+	            message.reply('You must be in a voice channel')
+	            return
+	        }
+
+	        voice.channel.join().then((connection) => {
+	            connection.play(path.join(__dirname, filepath))
+	        })
+    	}
+
     	if (message.content.includes('!!haachama cookpasta')){
-      		const { voice } = message.member
-
-      		if (!voice.channelID) {
-        		message.reply('You must be in a voice channel')
-        		return
-      		}
-
-      		voice.channel.join().then((connection) => {
-      			message.channel.send("Haachama cooking in 60s.")
-      			connection.play(path.join(__dirname, '../../assets/audio/hpasta1.mp3'))
-      		})
+    		message.channel.send("Haachama cooking in 60s.")
+    		playAudio('../../assets/audio/hpasta1.mp3')
 
       	//Haachama Song
 
-    	} else if (message.content.includes('!!haachama senbonzakura')){
-      		const { voice } = message.member
+    	} 
 
-      		if (!voice.channelID) {
-        		message.reply('You must be in a voice channel')
-        		return
-      		}
+    	else if (message.content.includes('!!haachama senbonzakura')){
+      		playAudio('../../assets/song/haachama/senbonzakura.mp3')
+    	} 
 
-      		voice.channel.join().then((connection) => {
-      			connection.play(path.join(__dirname, '../../assets/song/haachama/senbonzakura.mp3'))
-      		})
-    	} else if (message.content.includes('!!haachama shapeofyou')){
-      		const { voice } = message.member
+    	else if (message.content.includes('!!haachama shapeofyou')){
+      		playAudio('../../assets/song/haachama/shapeofyou.mp3')
 
-      		if (!voice.channelID) {
-        		message.reply('You must be in a voice channel')
-        		return
-      		}
+    	} 
 
-      		voice.channel.join().then((connection) => {
-      			connection.play(path.join(__dirname, '../../assets/song/haachama/shapeofyou.mp3'))
-      		})
-
-    	} else if (message.content.includes('!!haachama natsumatsuri')){
-      		const { voice } = message.member
-
-      		if (!voice.channelID) {
-        		message.reply('You must be in a voice channel')
-        		return
-      		}
-
-      		voice.channel.join().then((connection) => {
-      			connection.play(path.join(__dirname, '../../assets/song/haachama/natsumatsuri.mp3'))
-      		})
+    	else if (message.content.includes('!!haachama natsumatsuri')){
+      		playAudio('../../assets/song/haachama/natsumatsuri.mp3')
 
     	} 
     }
