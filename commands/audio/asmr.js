cc0a42a83d7be7da40b8f2cb397d6bbe8fd96ca6
shiro2332 <68656146+shiro2332@users.noreply.github.com>
2020-11-24 01:14:45 +0800

    321

---
+++ commands/audio/asmr.js
@@ -0,0 +1,26 @@
+const Commando = require('discord.js-commando')
+const path = require('path')
+
+module.exports = class PlayAudioCommand extends Commando.Command {
+  constructor(client) {
+    super(client, {
+      name: 'asmr',
+      group: 'audio',
+      memberName: 'asmr',
+      description: 'fbk',
+    })
+  }
+
+  async run(message) {
+    const { voice } = message.member
+
+    if (!voice.channelID) {
+      message.reply('You must be in a voice channel')
+      return
+    }
+
+    voice.channel.join().then((connection) => {
+      connection.play(path.join(__dirname, '../../assets/audio/asmr.mp3'))
+    })
+  }
+}
