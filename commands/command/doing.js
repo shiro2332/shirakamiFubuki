1547b17efc6ef52195aa98dcd5aa484f5d5d9bf3
shiro2332 <68656146+shiro2332@users.noreply.github.com>
2020-11-23 15:10:57 +0800

    fbk

--- commands/command/doing.js
+++ commands/command/doing.js
@@ -11,6 +11,6 @@ module.exports = class BasicCommand extends Command {
 	}
 
 	async run(message) {
-		return message.channel.send({files: ["./assets/images/chill.jpg"]})
+		return message.channel.send({files: ["./assets/images/fbk.gif"]})
 	}
 };
