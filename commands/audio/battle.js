7944a80d42972d645a7c1c8ca06550c54eac871d
shiro2332 <68656146+shiro2332@users.noreply.github.com>
2020-11-26 00:18:05 +0800

    dada

---
+++ commands/audio/battle.js
@@ -0,0 +1,26 @@
+const Commando = require('discord.js-commando')
+const path = require('path')
+
+module.exports = class PlayAudioCommand extends Commando.Command {
+  constructor(client) {
+    super(client, {
+      name: 'battle',
+      group: 'audio',
+      memberName: 'battle',
+      description: 'B A T T L E',
+    })
+  }
+
+  async run(message) {
+    const { voice } = message.member
+
+    if (!voice.channelID) {
+      message.reply('You must be in a voice channel')
+      return
+    }
+
+    voice.channel.join().then((connection) => {
+      connection.play(path.join(__dirname, '../../assets/audio/battle.mp3'))
+    })
+  }
+}
