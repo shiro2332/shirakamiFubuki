16a968d5ddf7fd6589e162d3f736ef271b8817a6
shiro2332 <68656146+shiro2332@users.noreply.github.com>
2020-11-25 23:45:24 +0800

    Update chorus command.js

--- commands/audio/chorus command.js
+++ commands/audio/chorus command.js
@@ -5,7 +5,7 @@ module.exports = class PlayAudioCommand extends Commando.Command {
     constructor(client) {
         super(client, {
             name: 'chorus',
-            group: 'audio',
+            group: 'hololive',
             memberName: 'chorus',
             description: 'Chorus Command',
         })
