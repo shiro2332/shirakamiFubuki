de1970bccf2f0024429a425913aa8eb789843791
TastyMeat <65907415+TastyMeat@users.noreply.github.com>
2020-12-12 22:20:51 +0800

    glhf

--- commands/audio/haachama command.js
+++ commands/audio/haachama command.js
@@ -2,97 +2,83 @@ const Commando = require('discord.js-commando')
 const path = require('path')
 
 module.exports = class PlayAudioCommand extends Commando.Command {
-  	constructor(client) {
-    	super(client, {
-	      	name: 'haachama',
-	      	group: 'hololive',
-	      	memberName: 'haachama',
-	      	description: 'Commands available to Haachama',
-    	})
-  	}
+	constructor(client) {
+		super(client, {
+			name: 'haachama',
+			group: 'hololive',
+			memberName: 'haachama',
+			description: 'Commands available to Haachama',
+		})
+	}
 
-  	async run(message) {
+	async run(message) {
 		const emoji = '<:haachama:779986363683569695> '
-  		function playAudio(filepath){
-	        const { voice } = message.member
+		const folderPath = '../../assets/song/haachama/'
 
-	        if (!voice.channelID) {
-	            message.reply('You must be in a voice channel')
-	            return
-	        }
+		function playAudio(filepath) {
+			const { voice } = message.member
+			if (!voice.channelID) {
+				message.reply('You must be in a voice channel')
+				return
+			}
 
-	        voice.channel.join().then((connection) => {
-	            connection.play(path.join(__dirname, filepath))
-	        })
-    	}
+			voice.channel.join().then((connection) => {
+				connection.play(path.join(__dirname, filepath))
+			})
+		}
 
-    	if (message.content.includes('cookpasta')){
-    		message.channel.send("Haachama cooking in 60s.")
-    		playAudio('../../assets/audio/hpasta1.mp3')
-    	}
+		//Songs
+		if (message.content.includes('senbonzakura'))
+			playAudio(folderPath + 'senbonzakura.mp3')
 
-    	if (message.content.includes('cookgyoza')){
-    		message.channel.send("Haachama cooking in 60s.")
-    		playAudio('../../assets/audio/hgyoza.mp3')
-    	}
+		else if (message.content.includes('shapeofyou'))
+			playAudio(folderPath + 'shapeofyou.mp3')
 
-    	if (message.content.includes('cookcurry')){
-    		message.channel.send("Haachama cooking in 60s.")
-    		playAudio('../../assets/audio/hcurry.mp3')
-    	}
+		else if (message.content.includes('natsumatsuri'))
+			playAudio(folderPath + 'natsumatsuri.mp3')
 
-      	//Haachama Song
+		else if (message.content.includes('polkka'))
+			playAudio(folderPath + 'polka.mp3')
 
-    	else if (message.content.includes('senbonzakura')){
-      		playAudio('../../assets/song/haachama/senbonzakura.mp3')
-    	} 
+		else if (message.content.includes('takemehome'))
+			playAudio(folderPath + 'takemehome.mp3')
 
-    	else if (message.content.includes('shapeofyou')){
-      		playAudio('../../assets/song/haachama/shapeofyou.mp3')
-    	} 
+		//Haachama Cooking
+		else if (message.content.includes('cookpasta')) {
+			message.channel.send("Haachama cooking in 60s.")
+			playAudio('../../assets/audio/hpasta1.mp3')
+		}
+		if (message.content.includes('cookgyoza')) {
+			message.channel.send("Haachama cooking in 60s.")
+			playAudio('../../assets/audio/hgyoza.mp3')
+		}
+		if (message.content.includes('cookcurry')) {
+			message.channel.send("Haachama cooking in 60s.")
+			playAudio('../../assets/audio/hcurry.mp3')
+		}
 
-    	else if (message.content.includes('natsumatsuri')){
-      		playAudio('../../assets/song/haachama/natsumatsuri.mp3')
-    	} 
+		else if (message.content.includes('help')) {
+			message.channel.send(emoji
+				+ "Commands available to Haachama:\n"
+				+ "!!haachama help : Show commands available to Haachama\n"
+				+ "\nSongs:\n"
+				+ "!!haachama senbonzakura : Play Senbonzakura\n"
+				+ "!!haachama shapeofyou : Play Shape of You\n"
+				+ "!!haachama natsumatsuri : Play Natsumatsuri\n"
+				+ "!!haachama takemehome : Play Take me Home\n"
+				+ "\nCookings:\n"
+				+ "!!haachama cookpasta : Haachama Cooking Pasta Guide\n"
+				+ "!!haachama cookgyoza : Haachama Cooking Gyoza Guide\n"
+				+ "!!haachama cookcurry : Haachama Cooking Curry Guide\n"
+			)
+		}
 
-    	else if (message.content.includes('polkka')){
-      		playAudio('../../assets/song/haachama/polka.mp3')
-    	}
+		else if (message.content === "!!haachama") {
+			//empty
+		}
 
-      else if (message.content.includes('takemehome')){
-          playAudio('../../assets/song/haachama/takemehome.mp3')
-      } 
-
-    	else if (message.content.includes('help')){
-      		message.channel.send(emoji 
-      			+ "Commands available to Haachama:\n" 
-      			+ "!!haachama help : Show commands available to Haachama\n" 
-      			+ "!!haachama cookpasta : Haachama Cooking Pasta Guide\n"
-      			+ "!!haachama cookgyoza : Haachama Cooking Gyoza Guide\n" 
-      			+ "!!haachama cookcurry : Haachama Cooking Curry Guide\n"  
-      			+ "!!haachama senbonzakura : Play Senbonzakura\n" 
-      			+ "!!haachama shapeofyou : Play Shape of You\n" 
-      			+ "!!haachama natsumatsuri : Play Natsumatsuri\n"
-            + "!!haachama takemehome : Play Take me Home\n"
-      			)
-    	}
-
-    	else if (message.content === "!!haachama"){
-            //empty
-        } 
-
-    	else {
-    		message.channel.send(emoji + "HAACHAMACHAMA~~~!!!")
-    	} 
-    }
-}	
-
-//Commands help
-/**
-Commands available to Haachama:
-!!haachama help : Show commands available to Haachama
-!!haachama cookpasta : Haachama Cooking Pasta Guide
-!!haachama senbonzakura : Play Senbonzakura
-!!haachama shapeofyou : Play Shape of You
-!!haachama natsumatsuri : Play Natsumatsuri
-**/
+		else {
+			message.channel.send(emoji + "HAACHAMACHAMA~~~!!!")
+		}
+	}
+}
--- commands/audio/kanata command.js
+++ commands/audio/kanata command.js
@@ -2,55 +2,49 @@ const Commando = require('discord.js-commando')
 const path = require('path')
 
 module.exports = class PlayAudioCommand extends Commando.Command {
-	  constructor(client) {
-		    super(client, {
-			      name: 'kanata',
-			      group: 'hololive',
-			      memberName: 'kanata',
-			      description: 'Commands available to Kanata',
-		    })
-	  }
+	constructor(client) {
+		super(client, {
+			name: 'kanata',
+			group: 'hololive',
+			memberName: 'kanata',
+			description: 'Commands available to Kanata',
+		})
+	}
 
-  	async run(message) {
-		  const { voice } = message.member
-		  const emoji = "<:kanata:782140207649521664> "
-	    function playAudio(filepath){
-	        if (!voice.channelID) {
-	            message.reply('You must be in a voice channel')
-	            return
-	        }
+	async run(message) {
+		const emoji = "<:kanata:782140207649521664> "
+		const folderPath = '../../assets/song/kanata/'
 
-	        voice.channel.join().then((connection) => {
-	            connection.play(path.join(__dirname, filepath))
-	        })
-	    }
+		function playAudio(filepath) {
+			const { voice } = message.member
+			if (!voice.channelID) {
+				message.reply('You must be in a voice channel')
+				return
+			}
 
-	    if (message.content.includes('chocolate')){
-	        playAudio('../../assets/song/watame/ChocolateBox.mp3')  	
-	    }
+			voice.channel.join().then((connection) => {
+				connection.play(path.join(__dirname, filepath))
+			})
+		}
 
-	    else if (message.content.includes('help')){
-	        message.channel.send(emoji
-		        	+ "Commands available to Kanata:\n" 
-		        	+ "!!kanata help : Show commands available to Kanata\n" 
-		        	+ "!!kanata chocolate : Play Chocolate Box" 
-		        	)
-	    }
+		if (message.content.includes('chocolate'))
+			playAudio('../../assets/song/watame/ChocolateBox.mp3')
 
-	    else if (message.content === "!!kanata"){
-	        //empty
-	    } 
+		else if (message.content.includes('help')) {
+			message.channel.send(emoji
+				+ "Commands available to Kanata:\n"
+				+ "!!kanata help : Show commands available to Kanata\n"
+				+ "\nSongs:\n"
+				+ "!!kanata chocolate : Play Chocolate Box"
+			)
+		}
 
-	    else {
-	        message.channel.send(emoji + "Eh?")
-	    }
+		else if (message.content === "!!kanata") {
+			//empty
+		}
+
+		else {
+			message.channel.send(emoji + "Eh?")
+		}
 	}
 }
-
-//Commands help
-/**
-Commands available to Kanata:
-!!kanata help : Show commands available to Kanata
-!!kanata chocolate: Play Chocolate Box
-
-**/
