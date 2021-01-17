98cf8e72f71ec8302833af380c07cc200fb5d4ec
shiro2332 <68656146+shiro2332@users.noreply.github.com>
2021-01-17 21:44:45 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-//const Commando = require('discord.js-commando')
+const { Commando } = require('discord.js-commando')
 const path = require('path')
 const information = require('./Information.json')
 
