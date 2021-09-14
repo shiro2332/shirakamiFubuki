bac89c0e19356f4d17f72fb7cdea6dfbad5bda5f
Shiro <68656146+shiro2332@users.noreply.github.com>
2021-09-14 11:10:25 +0800

    Update youtube-music.js

--- commands/audio/youtube-music.js
+++ commands/audio/youtube-music.js
@@ -23,29 +23,28 @@ module.exports = class PlayAudioCommand extends Commando.Command {
 		else {
 			message.say(`Invalid argument. Use \`//youtube help\` to help you.`);
 		}
-	}
-}
 
-function oneArgumentCommand(message, args) {
 
-}
+    function oneArgumentCommand(message, args) {
 
-function twoArgumentCommand(message, args) {
-	if (args[0] === 'play') {
-		const { voice } = message.member;
-	
-		if (!voice.channelID) {
-			message.reply(`you must join a voice channel first.`);
-			return;
-		}
-			
-		voice.channel.join().then(connection => {
-			const dispatcher = connection.play(ytdl(args[1]));
-			dispatcher.on('end', end => {
-				voice.channel.leave();
-			});
-		});
-	}
+    }
+
+    function twoArgumentCommand(message, args) {
+        if (args[0] === 'play') {
+            const { voice } = message.member;
+        
+            if (!voice.channelID) {
+                message.reply(`you must join a voice channel first.`);
+                return;
+            }
+                
+            voice.channel.join().then(connection => {
+                const dispatcher = connection.play(ytdl(args[1]));
+                dispatcher.on('end', end => {
+                    voice.channel.leave();
+                });
+            });
+        }
+    }
 }
 }
- 
