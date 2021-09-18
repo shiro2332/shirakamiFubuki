fc2a77bd7ccc466c68ac512de70826cd54c24ed1
Shiro <68656146+shiro2332@users.noreply.github.com>
2021-09-18 18:04:04 +0800

    Update youtube-music.js

--- commands/audio/youtube-music.js
+++ commands/audio/youtube-music.js
@@ -20,7 +20,7 @@ module.exports = class PlayAudioCommand extends Commando.Command {
         }
 
         voice.channel.join().then((connection) => {
-            connection.play(ytdl(args)
+            connection.play(ytdl(args))
         })
 
     }
